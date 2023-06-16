ba127acb0dd546f86b753dacff65fe7ed3dd62d5
stingojohn <olumekokayode@gmail.com>
2023-06-16 16:49:57 -0700

    Low level programming

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,30 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Succes)
+ * main-program entry point.
+ * Return:0 success, non zero error.
  */
 
-int main(void)
+  int main(void)
 {
-	int n;
+	int num;
 
-	for (n = 0 ; n < 10 ; n++)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar(n + '0');
-		if (n < 9)
-		{
-			putchar(',');
-			putchar(32);
-		}
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
 	}
+
 	putchar('\n');
+
 	return (0);
 }
-
